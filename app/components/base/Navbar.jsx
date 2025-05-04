41188e2dc3174f6561ab7af13374b16553e14ee9
David <dddavvviddd000@gmail.com>
2025-05-04 20:33:11 +0200

    Añadidas subrutas al navbar de teléfono

--- app/components/base/Navbar.jsx
+++ app/components/base/Navbar.jsx
@@ -50,12 +50,50 @@ export default function Navbar(){
         document.getElementById('mobNav').classList.add('translate-x-[-100%]');
     }
 
-    // Añadir el event listener que llama a la función hideNav cuando se pierde el focus del botón de menú
-    useEffect(() => {
-        const bars = document.getElementById('bars');
-        bars.addEventListener('blur', hideNav);
+    // Función para mostrar el nav de pantallas pequeñas
+    function showNav(){
+        document.getElementById('mobNav').classList.remove('translate-x-[-100%]');
+    }
 
-        return () => {bars.removeEventListener('blur', hideNav)}
+    // Función para desplegar y ocultar los sublinks del menú del móvil
+    function toggleSubLinks(id, event){
+        document.getElementById(`mobSubLinks${id}`).classList.toggle('hidden');
+        event.target.classList.toggle('rotate-180');
+    }
+
+    // Añadir el event listener que llama a la función hideNav cuando se pierde el focus del menú
+    useEffect(() => {
+        //
+        //Declaro variables
+        //Botón del menú
+        const bars = document.getElementById('bars');
+        //Menú de móviles
+        const mobNav = document.getElementById('mobNav');
+        //Todos los elementos descendientes del menú
+        const subMobNav = document.querySelectorAll('#mobNav *');
+        //Event listener para cerrar el menú cuando pierde el focus
+        bars.addEventListener('blur', hideNav);
+        //Poner el foco en el menú cuando se hace click en el botón del menú
+        mobNav.addEventListener('focus', focusBars)
+
+        //Función para establecer el focus en el menú
+        function focusBars(){
+            showNav();
+            bars.focus();
+        }
+        //Establecer el focus en el menú cuando se hace click en cualquier descendiente
+        subMobNav.forEach((element) => {
+            element.addEventListener('focus', focusBars);
+        });
+
+        //Eliminar todos los event listeners
+        return () => {
+            bars.removeEventListener('blur', hideNav);
+            mobNav.removeEventListener('click', focusBars);
+            subMobNav.forEach((element) => {
+                element.removeEventListener('click', focusBars);
+            });
+        }
 
     }, []);
 
@@ -79,19 +117,19 @@ export default function Navbar(){
                 <h1 className=" text-xl md:text-2xl lg:text-3xl font-bold">David Safar Cano</h1>
             </div>
 
-            <nav className="hidden md:block items-center absolute right-10 w-2/5 text-green-400">
+            <nav className="hidden md:block items-center absolute right-10 md: md:w-3/5 lg:w-2/5 text-green-400">
                 <ul className="flex justify-around text-base md:text-lg lg:text-xl">
                     {
                         navLinks.map((link) => {
                             return (
-                                <li key={link.id} className="relative group">
+                                <li key={link.id} className="relative text-center group">
                                     <Link className="block h-[100%] w-[100%]" href={link.href}>{link.name}</Link>
                                     {link.sub && <ul className="shadow absolute top-[100%] left-[50%] hidden translate-x-[-50%] group-hover:block bg-[#fff] rounded-sm">
                                         {
                                             link.sub.map((sublink, index) => {
                                                 return (
-                                                    <li key={sublink.id} className={`mt-1 ${index == 0 || 'border-t-1'}`}>
-                                                        <Link className="block h-[100%] w-[100%] py-1 px-3" href={`${sublink.href.startsWith('/') || link.href + '/'}${sublink.href}`}>{sublink.name}</Link>
+                                                    <li key={sublink.id} className={`hover:bg-[#f1f1f1] ${index == link.sub.length - 1 || 'border-b-1'}`}>
+                                                        <Link className="block h-[100%] w-[100%] py-2 px-3" href={`${sublink.href.startsWith('/') ? '' : link.href + '/'}${sublink.href}`}>{sublink.name}</Link>
                                                     </li>
                                                 )
                                             })
@@ -103,13 +141,30 @@ export default function Navbar(){
                     }
                 </ul>
             </nav>
-            <nav id="mobNav" className="md:hidden transition-transform duration-500 fixed left-0 translate-x-[-100%] top-18 w-[30vw] text-center shadow-sm">
+            <nav tabIndex="0" id="mobNav" className="md:hidden transition-transform duration-500 fixed left-0 translate-x-[-100%] top-18 w-[30vw] text-center shadow-sm">
                 <ul>
                 {
-                        navLinks.map((link) => {
+                        navLinks.map((link, index) => {
                             return (
                                 <li key={link.id} className="border-b-gray-600 border-b-1 bg-white relative">
-                                    <Link href={link.href}>{link.name}</Link>
+                                    <div className="flex h-10 items-center">
+                                        <Link className="flex items-center justify-center h-[100%] w-[100%]" href={link.href}>{link.name}</Link>
+                                        <div className="flex items-center justify-center h-[100%] w-10" onClick={(event) => {link.sub && toggleSubLinks(index, event)}}>
+                                            {link.sub && <i className="fas fa-caret-down"></i>}
+                                        </div>
+                                    </div>
+                                    
+                                    {link.sub && <ul id={`mobSubLinks${index}`} className="shadow group-hover:block bg-[#fff] rounded-sm hidden">
+                                        {
+                                            link.sub.map((sublink, index) => {
+                                                return (
+                                                    <li key={sublink.id} className={`hover:bg-[#f1f1f1] bg-[#f5f5f5] ${index == link.sub.length - 1 || 'border-b-1'}`}>
+                                                        <Link className="block h-[100%] w-[100%] py-1 px-3" href={`${sublink.href.startsWith('/') ? '' : link.href + '/'}${sublink.href}`}>{sublink.name}</Link>
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>}
                                 </li>
                             );
                         })
